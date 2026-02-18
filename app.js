52744027303b369a3e8162187dd13a5391dc3ff3
samhegland-design <sam.hegland@gmail.com>
2026-02-18 14:46:34 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -192,6 +192,6 @@ function displayBreakdownChart(data) {
 
 function formatCurrency(value) {
     const num = parseFloat(value);
-    if (isNaN(num)) return '$0.00';
-    return '$' + num.toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2});
+    if (isNaN(num)) return '$0';
+    return '$' + num.toLocaleString('en-US', {minimumFractionDigits: 0, maximumFractionDigits: 0});
 }
